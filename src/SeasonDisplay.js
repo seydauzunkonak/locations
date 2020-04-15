05ac35887bb01411e096b8121f15bde09dc08c7c
seydauzunkonak <52824376+seydauzunkonak@users.noreply.github.com>
2020-04-16 02:14:37 +0300

    geolocation

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,39 @@
 import React, { Component } from "react";
 
 class SeasonDisplay extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      long: 0,
+      error: "",
+    };
+  }
   render() {
-    return <div></div>;
+    // o an ki konumu bulma
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+        // konumu güncelleme
+        this.setState({
+          long: position.coords.longitude,
+        });
+      },
+      (err) => {
+        console.log(err);
+        this.setState({
+          error: "kullanıcı lokasyon bilgisi paylaşmak istemiyor",
+        });
+      }
+    );
+    return (
+      <div>
+        long : {this.state.long}
+        <br />
+        error : {this.state.error}
+      </div>
+    );
   }
 }
+
 export default SeasonDisplay;
--- src/Spinner.js
+++ src/Spinner.js
@@ -1,11 +1,2 @@
-import React, { Component } from 'react'
 
- class Spinner extends Component {
-    render() {
-        return (
-          
-            )
-    }
-}
-export default Spinner ;
 
