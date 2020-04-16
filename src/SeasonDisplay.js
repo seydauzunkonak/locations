213c75cca96b257070adc0b820e3b2b815b73af1
seydauzunkonak <52824376+seydauzunkonak@users.noreply.github.com>
2020-04-17 02:15:29 +0300

    adding icon

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -5,18 +5,16 @@ class SeasonDisplay extends Component {
     super(props);
 
     this.state = {
-      long: 0,
+      lat: null,
       error: "",
     };
-  }
-  render() {
     // o an ki konumu bulma
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         console.log(position);
         // konumu güncelleme
         this.setState({
-          long: position.coords.longitude,
+          lat: position.coords.latitude,
         });
       },
       (err) => {
@@ -26,13 +24,57 @@ class SeasonDisplay extends Component {
         });
       }
     );
-    return (
-      <div>
-        long : {this.state.long}
-        <br />
-        error : {this.state.error}
-      </div>
-    );
+  }
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+  SeasonDisplay(lat) {
+    const currentMonth = new Date().getMonth();
+    if (lat < 0) {
+      //guney yarımküre
+      if (currentMonth > 3 && currentMonth < 8) {
+        return {
+          text : "Snowboard Season"
+          iconName : 'snowflake'
+        }
+      } else {
+        return {
+          text : "Summer Season"
+          iconName : 'sun'
+        }
+    } else {
+      if (currentMonth > 8 || currentMonth < 3) {
+        return "Snowboard Season";
+      } else {
+        return "Summer Season";
+      }
+      //kuzey yarımküre
+    }
+  }
+
+  render() {
+    const { lat, error } = this.state;
+
+    if (lat !== 0 && !error) {
+      const season = this.SeasonDisplay(lat);
+      // season javascript değişkeniolduğu için {}
+      return (
+        <div>
+          <h2 className="ui header">
+            <i class="snowflake outline icon"></i>
+            <i class="sun outline icon"></i>
+            <div className=" content">{season}</div>
+          </h2>
+        </div>
+      );
+    } else if (lat === 0 && error) {
+      return <div>Error : {error}</div>;
+    }
+
+    return <div>"Loading ..."</div>;
   }
 }
 
